f5d5d1dfc6ad83f41881c55fc74d3df3700cd442
victorkwan <victorkwan@ucla.edu>
2014-04-03 00:24:59 -0700

    Added includes, static function to find command type

--- Lab 1/lab1-Skeleton/read-command.c
+++ Lab 1/lab1-Skeleton/read-command.c
@@ -3,57 +3,77 @@
 #include "command.h"
 #include "command-internals.h"
 
+#include <ctype.h>
 #include <error.h>
+#include <string.h>
 
 /* FIXME: You may need to add #include directives, macro definitions,
-   static function definitions, etc.  */
+ static function definitions, etc.  */
 
 /* FIXME: Define the type 'struct command_stream' here.  This should
-   complete the incomplete type declaration in command.h.  */
+ complete the incomplete type declaration in command.h.  */
 
 struct command_stream
 {
-  command* stream;
-} cstream;
+    command* s;
+} stream;
+
+
+/* Returns true or false depending on whether t has changed. Additional
+    logic is required for OR/PIPE distinctions because |, ||. We use this
+    function to break between commands. */
+
+static
+bool set_command_type(command_type_t* t, char c)
+{
+    if(ischar(c) && *t != SIMPLE_COMMAND)
+        *t = SIMPLE_COMMAND;
+    else if(c == '&' && *t != AND_COMMAND)
+        *t = AND_COMMAND;
+    else if(c == ';' && *t != SEQUENCE_COMMAND)
+        *t = SEQUENCE_COMMAND;
+    else if(c == '|' && *t != OR_COMMAND)
+        *t = OR_COMMAND;
+    else if(c == '(' && *t != SUBSHELL_COMMAND)
+        *t = SUBSHELL_COMMAND;
+    else
+        return false;
+
+    return true;
+}
 
 /* Create a command stream from GETBYTE and ARG.  A reader of
-     the command stream will invoke GETBYTE (ARG) to get the next byte.
-     GETBYTE will return the next input byte, or a negative number
-     (setting errno) on failure.  */
-command_stream_t
-make_command_stream (int (*get_next_byte) (void *),
-		     void *get_next_byte_argument)
-{
-  /* FIXME: Replace this with your implementation.  You may need to
+ the command stream will invoke GETBYTE (ARG) to get the next byte.
+ GETBYTE will return the next input byte, or a negative number
+ (setting errno) on failure.  */
+ command_stream_t
+ make_command_stream (int (*get_next_byte) (void *),
+   void *get_next_byte_argument)
+ {
+    /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-    
-    // david|
-    // daviide<
 
-     char c;
-     long int k=0;
-     command_type t;
-     while((c = get_next_byte(get_next_byte_argument)) > 0)
-     {
-
-
-        // 1. Process commands
-        // 2. Processing string
-        // 3. Process subshell
+    char c;
+    long int k=0;
+    command_type_t t;
+    while((c = get_next_byte(get_next_byte_argument)) > 0)
+    {
+/*      
+*/
         k++;
-     }
-  
-  error (1, 0, "command reading not yet implemented");
-  return 0;
+    }
+    
+    error (1, 0, "command reading not yet implemented");
+    return 0;
 }
 
 /* Read a command from STREAM; return it, or NULL on EOF.  If there is
-   an error, report the error and exit instead of returning.  */
+ an error, report the error and exit instead of returning.  */
 command_t
 read_command_stream (command_stream_t s)
 {
-  /* FIXME: Replace this with your implementation too.  */
-  error (1, 0, "command reading not yet implemented");
-  return 0;
+    /* FIXME: Replace this with your implementation too.  */
+    error (1, 0, "command reading not yet implemented");
+    return 0;
 }
