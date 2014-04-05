05bb47e772d9bf2e90414826374aef0b7189c420
victorkwan <victorkwan@ucla.edu>
2014-04-05 13:46:40 -0700

    Tokenizer complete!
    Call the get_tokens function to get a dynamically allocated array of
    tokens. Additional notes:
        1. Newlines are all enveloped into a single token
        2. OR_STATE covers both | and ||

--- Lab 1/lab1-Skeleton/read-command.c
+++ Lab 1/lab1-Skeleton/read-command.c
@@ -3,13 +3,129 @@
 #include "command.h"
 #include "command-internals.h"
 
-#include <error.h>
+#include <ctype.h>
+// #include <error.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
+
+struct command_stream {
+    command_t* command;
+} stream;
+
+typedef enum {
+    SIMPLE_STATE, AND_STATE, OR_STATE,
+        NEWLINE_STATE, NULL_STATE
+} state;
+
+static
+bool needs_splitting(state* s, char c)
+{
+    if(c == ' ' || c == '<' || c == '>' || c == '(' ||
+            c == ')' || c == ';')
+    {
+        *s = NULL_STATE;
+        return true;
+    }
+    else if((isalnum(c) || c == '!' || c == '%' || c == '+' ||
+                c == ',' || c == '-' || c == '.' || c == '/' ||
+                c == ':' || c == '@' || c == '^' || c == '_') && 
+                *s != SIMPLE_STATE)
+    {
+        *s = SIMPLE_STATE;
+        return true;
+    }
+    else if(c == '&' && *s != AND_STATE)
+    {
+        *s = AND_STATE;
+        return true;
+    }
+    else if(c == '|' && *s != OR_STATE)
+    {
+        *s = OR_STATE;
+        return true;
+    }
+    else if(c == '\n' && *s != NEWLINE_STATE)
+    {
+        *s = NEWLINE_STATE;
+        return true;
+    }
+    else
+        return false;
+}
+
+
+static
+char **get_tokens(int (*get_next_byte) (void *), 
+                    void *get_next_byte_argument)
+{
+    char c;
+    long int tokens_position = 0;
+    long int tokens_size = 1024;
+    char **tokens = (char **)malloc(sizeof(char *)*tokens_size);
+    state s = NULL_STATE;
+
+    long int buf_position = 0;
+    long int buf_size = 216;
+    char *buf = (char *)malloc(sizeof(char)*buf_size);
+
+    while((c = get_next_byte(get_next_byte_argument)) > 0)
+    {
+        if(!needs_splitting(&s, c))
+        {
+            if(buf_position == buf_size)
+            {
+                buf_size *= 2;
+                buf = (char *)realloc((void *)buf, sizeof(char)*buf_size);
+            }
+
+            buf[buf_position] = c;
+            buf_position++;
+        }
+        else
+        {
+            if(buf_position > 0)
+            {
+                char *tmp = (char *)malloc(sizeof(char)*buf_position);
+                long int i;
+                for(i = 0; i < buf_position; i++)
+                    tmp[i] = buf[i];
+                tmp[i] = '\0';
+
+                if(tokens_position == tokens_size)
+                {
+                    tokens_size *= 2;
+                    tokens = (char **)realloc((void *)tokens,
+                        sizeof(char *)*tokens_size);
+                }
+
+                tokens[tokens_position] = tmp;
+                tokens_position++;
+            }
+
+            buf_position = 0;
+
+            if(c != ' ')
+            {
+                buf[buf_position] = c;
+                buf_position++;
+            }
+        }
+    }
+
+    long int j;
+    for(j = 0; j < tokens_position; j++)
+    {
+        printf("%lu: %s\n", j, tokens[j]);
+    }
+
+    return tokens;
+}
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
@@ -18,7 +134,10 @@ make_command_stream (int (*get_next_byte) (void *),
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command reading not yet implemented");
+  // error (1, 0, "command reading not yet implemented");
+
+    get_tokens(get_next_byte, get_next_byte_argument);
+
   return 0;
 }
 
@@ -26,6 +145,6 @@ command_t
 read_command_stream (command_stream_t s)
 {
   /* FIXME: Replace this with your implementation too.  */
-  error (1, 0, "command reading not yet implemented");
+  // error (1, 0, "command reading not yet implemented");
   return 0;
 }
