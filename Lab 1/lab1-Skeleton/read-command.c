2b990d246f12d074cdcf6de110815a77b277f39e
victorkwan <victorkwan@ucla.edu>
2014-04-06 13:15:50 -0700

    Stack structures. Started the populate_stacks.

--- Lab 1/lab1-Skeleton/read-command.c
+++ Lab 1/lab1-Skeleton/read-command.c
@@ -15,19 +15,61 @@
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
 
-struct command_stream {
-    command_t* command;
-} stream;
-
 typedef enum {
-    SIMPLE_STATE, AND_STATE, OR_STATE,
-        INPUT_STATE, OUTPUT_STATE, OPEN_SUBSHELL_STATE,
-        CLOSE_SUBSHELL_STATE, SEQUENCE_STATE, NEWLINE_STATE, 
-        COMMENT_STATE, NULL_STATE
+    SIMPLE_STATE, INPUT_STATE, OUTPUT_STATE, AND_STATE, OR_STATE,
+        OPEN_SUBSHELL_STATE, CLOSE_SUBSHELL_STATE, SEQUENCE_STATE,
+        NEWLINE_STATE, COMMENT_STATE, NULL_STATE
 } state;
 
+struct command_stream {
+    command_t head;
+} stream;
+
+struct node {
+    command_t command;
+    struct node *next;
+};
+
+struct stack {
+    struct node *head;
+};
+
 static
-bool needs_splitting(state* s, char c)
+void init_stack(struct stack *s)
+{
+    s->head = NULL;
+}
+
+static
+void push(struct stack *s, command_t c)
+{
+    struct node *n = (struct node *)malloc(sizeof(struct node));
+    n->command = c;
+    n->next = s->head;
+
+    if(s->head == NULL)
+        s->head = n;
+    else
+        s->head = n;
+}
+
+
+static
+command_t pop(struct stack *s)
+{
+    if(s->head == NULL)
+        return NULL;
+
+    struct node *n = s->head;
+    s->head = n->next;
+
+    command_t c = n->command;
+    free(n);
+    return c;
+}
+
+static
+bool needs_splitting(state *s, char c)
 {
     if(c == ' ')
     {
@@ -91,20 +133,47 @@ bool needs_splitting(state* s, char c)
         return false;
 }
 
+/* Returns 0 if cmd->cmd, 1 if cmd->op, -1 if op->cmd */
 
 static
-int validate_token(char *token)
+int get_state(state *s, char c)
+{
+    state s_old = *s;
+    needs_splitting(s, c);
+
+    int a = s_old/3;
+    int b = (*s)/3;
+
+    if(a == b)
+        return 0;
+    else if(a < b)
+        return 1;
+    else
+        return -1;
+}
+
+static
+long int get_token_length(char *t)
+{
+    long int k;
+    for(k = 0; t[k] != '\0'; k++) {}
+    return k;
+}
+
+
+static
+long int validate_token(char *token)
 {
     state s;
     char c0 = token[0];
     needs_splitting(&s, c0);
 
-    if(s == AND_STATE || s == OR_STATE)
+    if(s == AND_STATE || s == OR_STATE || s == NEWLINE_STATE)
     {
         long int k;
         for(k = 0; token[k] != '\0'; k++)
         {
-            if(k == 2)
+            if(k == 2 && s != NEWLINE_STATE)
             {
                 fprintf(stderr, "Syntax error: {|...|, &...&}\n");
                 exit(1);
@@ -190,6 +259,42 @@ char **get_tokens(int (*get_next_byte) (void *),
 }
 
 
+/* We're going to skimp out on having two separate stack classes here:
+    a command with command_type SUBSHELL_COMMAND can fall under two categories.
+        1. status = 0 -> '('
+        2. status = 1 -> ')' */
+
+/*  SIMPLE_STATE, INPUT_STATE, OUTPUT_STATE, AND_STATE, OR_STATE,
+    OPEN_SUBSHELL_STATE, CLOSE_SUBSHELL_STATE, SEQUENCE_STATE,
+    NEWLINE_STATE, COMMENT_STATE, NULL_STATE */
+
+static
+void populate_stacks(char **tokens, long int ntokens,
+    struct stack *op, struct stack *cmd)
+{
+    state s = NULL_STATE;
+    command_t c = malloc(sizeof(struct command));
+
+    long int i;
+    for(i = 0; i < ntokens; i++)
+    {
+        state old_s = s;
+        int delta = get_state(&s, tokens[i][0]);
+        
+        if(delta == 0)
+        {
+            
+        }
+
+
+
+        
+
+
+    }
+}
+
+
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
              void *get_next_byte_argument)
@@ -202,12 +307,29 @@ make_command_stream (int (*get_next_byte) (void *),
     char **tokens = get_tokens(get_next_byte,
         get_next_byte_argument, &ntokens);
 
+    struct stack op;
+    init_stack(&op);
 
+    struct stack cmd;
+    init_stack(&cmd);
+
+    populate_stacks(tokens, ntokens, &op, &cmd);
+    
+    char hello[] = "helloes\n";
+    command_t c = (command_t)malloc(sizeof(struct command));
+    c->input = hello;
+
+    push(&op, c);
+    command_t d = pop(&op);
+    printf("%s", d->input);
+
+    /*
     long int j;
     for(j = 0; j < ntokens; j++)
     {
         printf("%lu: %s\n", j, tokens[j]);
     }
+    */
 
   return 0;
 }
