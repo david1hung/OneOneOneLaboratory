8ac9cb7b09537ec4ebf5385d4e13007e71862dd8
victorkwan <victorkwan@ucla.edu>
2014-04-22 08:53:47 -0700

    Interesting test cases

--- Lab 1/lab1-Skeleton/test-parallelism-splitter.sh
+++ Lab 1/lab1-Skeleton/test-parallelism-splitter.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-echo 'echo hello > a.txt && echo world > b.txt
-cat a.txt | sort > c.txt
-cat c.txt > d.txt
-cat d.txt' > parallelism_splitter
+echo 'od -tf -N 16000000 /dev/urandom > /dev/null
+od -tf -N 8000000 /dev/urandom > c.txt
+od -tf -N 4000000 /dev/urandom > /dev/null' > parallelism_splitter
 
-./timetrash -t parallelism_splitter
-rm parallelism_splitter
+printf '#1: Sans parallelism\n'
+time -p ./timetrash parallelism_splitter
+printf '\n#2: Parallelism with dependencies\n'
+time -p ./timetrash -t parallelism_splitter
+
+echo 'od -tf -N 16000000 /dev/urandom > a.txt
+od -tf -N 8000000 /dev/urandom > b.txt
+od -tf -N 4000000 /dev/urandom > c.txt' > parallelism_splitter
+
+printf '\n#3: Parallelism sans dependencies\n'
+time -p ./timetrash -t parallelism_splitter
+
+rm parallelism_splitter a.txt b.txt c.txt
