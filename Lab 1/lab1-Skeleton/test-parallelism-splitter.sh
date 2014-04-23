1183adf77410707cb947b25b2d60cbaccab659c5
victorkwan <victorkwan@ucla.edu>
2014-04-22 21:59:18 -0700

    more parallelism splitter stuff

--- Lab 1/lab1-Skeleton/test-parallelism-splitter.sh
+++ Lab 1/lab1-Skeleton/test-parallelism-splitter.sh
@@ -3,8 +3,9 @@
 result="test-parallelism-$USER.txt"
 
 echo 'od -tf -N 16000000 /dev/urandom > /dev/null
-od -tf -N 32000000 /dev/urandom > c.txt
-od -tf -N 16000000 /dev/urandom > /dev/null' > parallelism_splitter
+od -tf -N 24000000 /dev/urandom > c.txt
+od -tf -N 16000000 /dev/urandom > /dev/null
+od -tf -N 8000000 /dev/urandom > c.txt' > parallelism_splitter
 
 printf '#1: Sans parallelism\n' > $result
 { time -p ./timetrash parallelism_splitter ; } 2>> $result
